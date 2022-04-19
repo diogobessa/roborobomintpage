a4a2c288e9fd7f887c4a3f145ec1fe75dff40e2e
Diogo Bessa <diogobessawd@gmail.com>
2022-04-20 00:25:24 +0100

    last changes

--- src/components/MintButton/MintButton.js
+++ src/components/MintButton/MintButton.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
 import "./MintButton.css";
 
 import Button from '@mui/material/Button';
@@ -6,24 +6,46 @@ import InputLabel from '@mui/material/InputLabel';
 import MenuItem from '@mui/material/MenuItem';
 import FormControl from '@mui/material/FormControl';
 import Select, { SelectChangeEvent } from '@mui/material/Select';
+import Alert from '@mui/material/Alert';
+import { Snackbar } from "@mui/material";
+
 
 const MintButton = props => {
+    const [openToaster, setOpenToaster] = useState(false);
+    const [loading, setLoading] = useState(false);
     const nftMakerBaseUrl = "https://payment.nft-maker.io/";
     const projectId = "ROBO1020ID";
+    const wallet = "addr1q8ev22c7gehvarwp5yvd24ycefdweajefdx6zxqh0a4s3yyuhejpgz47vqm8xcsq527lds2vs5an5y0sa0yp4gxfu45qf055hp";
 
-    const openNftMakerPage = () => {
-        window.open(`${nftMakerBaseUrl}?p=${projectId}&c=${props.quantity}`, "_blank")
+
+    const copyMintAddress = async () => {
+        setLoading(true);
+        await navigator.clipboard.writeText(wallet);
+        setOpenToaster(true);
+        setLoading(false);
+        //window.open(`${nftMakerBaseUrl}?p=${projectId}&c=${props.quantity}`, "_blank")
     }
 
-    
 
-    const handleChange = (event: SelectChangeEvent) => {
-      props.changeMintQuantity(event.target.value);
-    };
+    const handleClose = () => {
+        setOpenToaster(false);
+    }
 
     return (
-    <Fragment>
-        <div className="quantity-price"> 
+        <Fragment>
+            <p className="mint-address">{wallet}</p>
+            <div className="mint-button"> 
+                <Button onClick={ copyMintAddress } variant="contained" disabled={loading}>{ loading ? 'a copiar...' : 'Copy Mint Address!' }</Button>
+                <Snackbar open={openToaster} anchorOrigin={{ vertical: 'top', horizontal: 'center' }} autoHideDuration={3000} onClose={handleClose} message='Address copied to the clipboard!'><Alert severity="success">Address copied to the clipboard!</Alert></Snackbar>
+            </div>  
+        </Fragment>
+    );
+  };
+  
+  export default MintButton;
+
+  /*
+         <div className="quantity-price"> 
             <FormControl fullWidth>
                 <InputLabel id="select-quantity">Quantity</InputLabel>
                 <Select
@@ -42,15 +64,7 @@ const MintButton = props => {
                 {50*props.quantity} ADA
             </p>
         </div>
-        <div className="mint-button"> 
-            <Button onClick={ openNftMakerPage } variant="contained">Mint!</Button>
-        </div>
-    </Fragment>
-    );
-  };
-  
-  export default MintButton;
-
+  */
   /*
   <button onClick={ openNftMakerPage }> Mint { props.quantity } ROBOROBO </button>
   */
